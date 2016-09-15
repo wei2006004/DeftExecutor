2dda19b105e412dce3143bd672923a69adac4fb5
wei <wei2006004@foxmail.com>
2016-09-15 15:47:17 +0800

    init demo lib

---
+++ deft-executor-demo/src/main/java/com/deft/example/Main.java
@@ -0,0 +1,10 @@
+package com.deft.example;
+
+/**
+ * Created by wei on 16-9-15.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.print("hello world!");
+    }
+}
